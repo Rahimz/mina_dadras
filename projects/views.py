c498ba646bf9f655a51b60c4827e9142590db6a2
rahim <rahim.aghareb@gmail.com>
2024-10-20 09:08:07 +0330

    update projects page

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.utils.translation import gettext_lazy as _
 
-# Create your views here.
+from .models import Project
+
+def ProjectsView(request):
+    projects = Project.objects.all()
+    context = dict(
+        page_title=_("projects"),
+        nav='projects',
+        projects=projects,
+    )
+    return render (
+        request,
+        'projects/projects.html',
+        context
+    )
