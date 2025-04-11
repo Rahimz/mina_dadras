dd67f8d93e7aeefa4b9b1e13e207b76eae8e49d5
rahim <rahim.aghareb@gmail.com>
2025-04-11 07:59:42 +0330

    update about model

--- generals/models.py
+++ generals/models.py
@@ -5,12 +5,62 @@ from tools.models import TimeStampedModel
 
 
 class About(TimeStampedModel):
-    about = models.TextField(
-        _("About me")
-    )
     image = models.ImageField(
         _("Image"),
         upload_to='about/'
+    )
+    home_page_image = models.ImageField(
+        _("Image in home page"),
+        upload_to='about/',
+        null=True, 
+        blank=True
+    )
+    title_about = models.CharField(
+        max_length=150,
+        null=True, blank=True,
+    )
+    about = models.TextField(
+        _("About me")
+    )
+    cv_file = models.FileField(
+        upload_to='about/',
+        null=True,
+        blank=True
+    )
+    cv_file_button = models.CharField(
+        max_length=150,
+        null=True,
+        blank=True
+    )
+    
+    title_addition = models.CharField(
+        _("Additional skils title"),
+        max_length=150,
+        null=True, blank=True,
+    )
+    addition = models.TextField(
+        _("Additional skils"),
+        blank=True
+    )
+    addition_file = models.FileField(
+        upload_to='about/',
+        null=True,
+        blank=True
+    )
+    addition_file_button = models.CharField(
+        max_length=150,
+        null=True,
+        blank=True
+    )
+    
+    title_contact = models.CharField(
+        _("Contact title"),
+        max_length=150,
+        null=True, blank=True,
+    )
+    contact = models.TextField(
+        _("Contact"),
+        blank=True
     )
     def __str__(self):
         return self.about[:20]
